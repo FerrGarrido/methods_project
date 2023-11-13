10b2052233ace453bad70e17236ddac0d94298fb
Daniel Pineda <danwsp@outlook.com>
2023-11-12 23:01:38 -0600

    Funciona metodo de Gauss

--- src/Gauss.java
+++ src/Gauss.java
@@ -2,51 +2,47 @@ import java.util.Scanner;
 
 public class Gauss {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Ingrese el número de ecuaciones: ");
-        int n = input.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el número de ecuaciones:");
+        int n = scanner.nextInt();
+        double[][] matriz = new double[n][n+1];
+        double[] solucion = new double[n];
 
-        double[][] matriz = new double[n][n + 1];
+        System.out.println("Introduce los coeficientes de las ecuaciones y los términos independientes:");
         for (int i = 0; i < n; i++) {
-            System.out.println("Ingrese los coeficientes de la ecuación " + (i + 1) + ":");
             for (int j = 0; j <= n; j++) {
-                matriz[i][j] = input.nextDouble();
+                matriz[i][j] = scanner.nextDouble();
             }
         }
 
-        gauss(matriz, n);
-    }
-
-    public static void gauss(double[][] matriz, int n) {
         for (int i = 0; i < n; i++) {
-            // Buscar la fila máxima en la columna i
-            int maxFila = i;
-            for (int j = i + 1; j < n; j++)
-                if (Math.abs(matriz[j][i]) > Math.abs(matriz[maxFila][i]))
-                    maxFila = j;
-
-            // Intercambiar filas máximas e i
-            double[] temp = matriz[maxFila];
-            matriz[maxFila] = matriz[i];
-            matriz[i] = temp;
-
-            // Hacer todos los elementos debajo de este uno cero
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i+1; j < n; j++) {
                 double factor = matriz[j][i] / matriz[i][i];
-                for (int k = i; k <= n; k++)
+                for (int k = i; k <= n; k++) {
                     matriz[j][k] -= factor * matriz[i][k];
+                }
             }
         }
 
-        // Imprimir la matriz escalonada
         System.out.println("La matriz escalonada es:");
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= n; j++)
+            for (int j = 0; j <= n; j++) {
                 System.out.print(matriz[i][j] + " ");
+            }
             System.out.println();
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            double suma = 0;
+            for (int j = i+1; j < n; j++) {
+                suma += matriz[i][j] * solucion[j];
+            }
+            solucion[i] = (matriz[i][n] - suma) / matriz[i][i];
+        }
+
+        System.out.println("La solución es:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("x" + (i+1) + " = " + solucion[i]);
         }
     }
 }
-
-
-
